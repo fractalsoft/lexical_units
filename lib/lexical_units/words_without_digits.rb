4eb10dfa1e78dea01f5b61994fc9ed8c909688a8
Aleksander Malaszkiewicz <alek@fractalsoft.org>
2013-07-16 22:00:47 +0200

    Split words without digits

---
+++ lib/lexical_units/words_without_digits.rb
@@ -0,0 +1,16 @@
+# encoding: utf-8
+module LexicalUnits
+  # Split text into words without digits
+  #
+  #   self.words("Lorem 0 ipsum") #=> ["Lorem", "ipsum"]
+  #   self.words("Lorem ipsum 100") #=> ["Lorem", "ipsum"]
+  def self.words_without_digits(text)
+    LexicalUnits::words(text).delete_if { |word| numeric?(word) }
+  end
+
+  private
+    def self.numeric?(value)
+      return true if value =~ /^\d+$/
+      true if Float(value) rescue false
+    end
+end
