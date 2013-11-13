44f73044416c4e8ea1d1758dd0716e263b46feaa
Alex Malaszkiewicz <torrocus@users.noreply.github.com>
2013-11-13 23:01:56 +0100

    Clean up with rspec 3.0

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,8 @@
 require 'lexical_units'
 require 'coveralls'
 Coveralls.wear!
+require 'codeclimate-test-reporter'
+CodeClimate::TestReporter.start
 
 RSpec.configure do |config|
   config.treat_symbols_as_metadata_keys_with_true_values = true
